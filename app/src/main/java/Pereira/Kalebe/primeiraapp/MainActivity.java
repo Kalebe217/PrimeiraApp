60302b8396b44aa6b1d02871097ac8eed2942571
20221TIIMI0327 <kalebe21.55@gmail.com>
2024-03-26 09:13:52 -0300

    inicio dos comentarios

--- app/src/main/java/Pereira/Kalebe/primeiraapp/MainActivity.java
+++ app/src/main/java/Pereira/Kalebe/primeiraapp/MainActivity.java
@@ -8,19 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+// tela inicial do app
 public class MainActivity extends AppCompatActivity {
 
+    //ao abrir tela, o metodo onCreate sera invocado
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //cria os elementos de interface criados nos arquivos xml
         setContentView(R.layout.activity_main);
 
-        /*-----------------------------------------*/
-        /*encontra o botão da tela*/
+
+
+        //encontra/obtem o botao da tela atraves o id
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        //cria detector de click para o click no botao e ativa metodo onclick caso o botao seja acionado
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//acionado ao clicar o botao
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
                 String textoDigitado = etDigiteAqui.getText().toString();
                 Intent i = new Intent(MainActivity.this, nextActivity.class);
